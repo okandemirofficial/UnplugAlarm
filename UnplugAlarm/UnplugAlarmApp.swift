f96b964258de2cb1f6f40017778044076afddd23
Okan Demir <okandemir3358@gmail.com>
2025-12-04 13:55:59 +0300

    Initial Commit

---
+++ UnplugAlarm/UnplugAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnplugAlarmApp.swift
+//  UnplugAlarm
+//
+//  Created by okdemir on 4.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct UnplugAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnplugAlarmTests/UnplugAlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnplugAlarmTests.swift
+//  UnplugAlarmTests
+//
+//  Created by okdemir on 4.12.2025.
+//
+
+import Testing
+@testable import UnplugAlarm
+
+struct UnplugAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
